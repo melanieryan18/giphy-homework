accf268532756b46cc08eb17a63717cc49c5b6a7
Melanie Ryan <melaniehoperyan@gmail.com>
2018-11-21 15:43:43 -0800

    one more thing to do

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 
-$("button").on("click", function findGifs() {
+$(document).on("click", "button", function findGifs() {
     // creating a variable to plug new data-animal
     var topics = $(this).attr("data-topics");
     // , "cow", "pengiun", "sloth", "capybara", "hummingbird", "hawk", "armadillo", "naked mole rat", "dolphin", "lemur", 
@@ -10,52 +10,48 @@ $("button").on("click", function findGifs() {
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-        var imagesArr = response.data;
+        var animalGifs = response.data;
+        var rating = response.rating;
         $("#animal-gifs").empty();
-        // take all the fixed_height images from the response 
-        // and display on the page
-        for (var i = 0; i < imagesArr.length; i++) {
+        for (var i = 0; i < animalGifs.length; i++) {
             var img = $("<img>");
             img.addClass("gif-image");
-            img.attr("src", imagesArr[i].images.fixed_height_still.url);
-            img.attr("data-still", imagesArr[i].images.fixed_height_still.url);
-            img.attr("data-animate", imagesArr[i].images.fixed_height.url);
+            img.attr("src", animalGifs[i].images.fixed_height_still.url);
+            img.attr("data-still", animalGifs[i].images.fixed_height_still.url);
+            img.attr("data-animate", animalGifs[i].images.fixed_height.url);
             img.attr("data-state", "still");
             $("#animal-gifs").append(img);
+            $("#button data-topics").append(rating)
+
         }
-        $(document).on("click", ".gif-image", function (e) {
-            // e.preventDefault();
-            var state = $(this).attr("data-state");
-            var animateUrl = $(this).attr("data-animate");
-            var stillUrl = $(this).attr("data-still");
-            if (state === "still") {
-                $(this).attr("src", animateUrl);
-                $(this).attr("data-state", "animate");
-            } else {
-                $(this).attr("src", stillUrl);
-                $(this).attr("data-state", "still");
-            }
-        });
 
-
-
-
-        // $(document).on("click", ".gif-button", function (e) {
-        //     e.preventDefault();
-        //     var btnValue = $(this).attr("data-name");
-        //     // callAPI(btnValue);
-        // });
-
-
-        $("#animal-button").on("click", function () {
-            console.log(("#animal-button").val());
-            var creature = $("#animal-input").val().trim();
-            topics.push(creature);
-            a.text(topics);
-            // Adding the button to the buttons-view div
-            $("#animal-gifs").append(a);
-
-        });
     });
+
 });
 
+$(document).on("click", ".gif-image", function () {
+    var state = $(this).attr("data-state");
+    var animateUrl = $(this).attr("data-animate");
+    var stillUrl = $(this).attr("data-still");
+    if (state === "still") {
+        $(this).attr("src", animateUrl);
+        $(this).attr("data-state", "animate");
+
+    } else {
+        $(this).attr("src", stillUrl);
+        $(this).attr("data-state", "still");
+
+    }
+});
+
+
+$("#submit-button").on("click", function (event) {
+    event.preventDefault();
+    // console.log(("#new-input").val());
+    var creature = $("#new-input").val().trim();
+    // topics.push(creature);
+    // a.text(topics);
+    // Adding the button to the buttons-view div
+    $("#animal-container").append('<button data-topics="' + creature +'">' + creature +'</button>');
+
+});
